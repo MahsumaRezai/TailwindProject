c2cab6c896ff12af555e4ba3afae3df49de2f452
unknown <mahsuma.rezai8@gmail.com>
2024-01-08 04:25:58 -0800

    add form section

---
+++ src/componets/Form.js
@@ -0,0 +1,14 @@
+import { Fragment } from "react";
+import Card from "./Card";
+
+const From=(props)=>{
+    return (
+        <Fragment>
+            <Card>
+                
+            </Card>
+
+        </Fragment>
+    )
+}
+export default From;
