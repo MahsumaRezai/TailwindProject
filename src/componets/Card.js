9ae60eb1ef62259b236a195b9dc276811facccc0
unknown <mahsuma.rezai8@gmail.com>
2024-01-08 05:28:31 -0800

    add form style

--- src/componets/Card.js
+++ src/componets/Card.js
@@ -3,7 +3,7 @@ import React, { Fragment } from 'react';
 const Card = (props) => {
     return (
    <Fragment>
-     <div className="bg-blue-100 w-6/12 h-fit m-auto mt-40 ">
+     <div className="bg-blue-100 w-4/12 h-max m-auto mt-40 p-12 flex flex-row flex justify-center justify-items-center gap-2  ">
      {props.children}
 
      </div>
--- src/componets/Form.js
+++ src/componets/Form.js
@@ -5,7 +5,17 @@ const From=(props)=>{
     return (
         <Fragment>
             <Card>
-                <h1>dkdkdkdk</h1>
+            <div>
+            <form class="m-4 flex flex-col gap-5  rounded-xl">
+            <input class=" p-5  w-400 bg-white border-yellow-500
+           border-t border-b border-r border-l" placeholder="Name"/>       
+           <input class="  p-5  w-400 bg-white border-yellow-500 border-t border-l border-r border-b font-bold" placeholder="LastName"/>
+           <input class="  p-5  w-400 bg-white border-yellow-500 border-t border-b border-r border-l font-bold" placeholder="Email"/>
+          <button class="px-8 rounded-r-lg bg-yellow-400 text-gray-800 font-bold p-4 uppercase border-yellow-500
+           border-t border-b border-r">Subscribe</button>
+      </form>
+
+            </div>
             </Card>
 
         </Fragment>
