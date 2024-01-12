2fa450868d2be5ae483f6adc1b194344db7ecba4
unknown <mahsuma.rezai8@gmail.com>
2024-01-12 01:20:40 -0800

    learn border in taliwind

--- src/componets/Card.js
+++ src/componets/Card.js
@@ -3,7 +3,7 @@ import React, { Fragment } from 'react';
 const Card = (props) => {
     return (
    <Fragment>
-     <div className="bg-blue-100 w-4/12 h-max m-auto mt-40 p-12 flex flex-row flex justify-center justify-items-center gap-2  ">
+     <div className="bg-gradient-to-r from-violet-500 to-fuchsia-500 w-4/12 h-max m-auto mt-40 p-12 flex flex-row flex justify-center justify-items-center gap-2  ">
      {props.children}
 
      </div>
--- src/componets/Form.js
+++ src/componets/Form.js
@@ -6,11 +6,11 @@ const From=(props)=>{
         <Fragment>
             <Card>
             <div>
-            <form class="m-4 flex flex-col gap-5  rounded-xl">
-            <input class=" p-5  w-400 bg-white border-yellow-500
-           border-t border-b border-r border-l" placeholder="Name"/>       
-           <input class="  p-5  w-400 bg-white border-yellow-500 border-t border-l border-r border-b font-bold" placeholder="LastName"/>
-           <input class="  p-5  w-400 bg-white border-yellow-500 border-t border-b border-r border-l font-bold" placeholder="Email"/>
+            <form class="m-4 flex flex-col gap-5  rounded-xl font-mono italic">
+            <input class=" p-5  w-400 bg-fuchsia-100 border-yellow-500 rounded-xl
+            border-2 italic text-sm text-center text-red text-blue-600/100  border-double" placeholder="Name"/>       
+           <input class="  p-5  w-400 bg-fuchsia-100 border-yellow-500  border-double rounded-xl border-2  font-bold italic text-sm text-center text-red text-blue-600/100" placeholder="LastName"/>
+           <input class="  p-5  w-400 bg-fuchsia-100   border-2  border-double  border-yellow-500 rounded-xl  font-bold italic text-sm text-center text-red text-blue-600/100" placeholder="Email"/>
           <button class="px-8 rounded-r-lg bg-yellow-400 text-gray-800 font-bold p-4 uppercase border-yellow-500
            border-t border-b border-r">Subscribe</button>
       </form>
